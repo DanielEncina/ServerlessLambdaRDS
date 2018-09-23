f09b4d3cd6d0edcbcda6a5f917065732636b9820
Daniel Encina <dani.encina@alumnos.duoc.cl>
2018-09-22 21:53:23 -0300

    adding function to find one todo

--- crud-rds-mysql/crud/todos.js
+++ crud-rds-mysql/crud/todos.js
@@ -20,3 +20,23 @@ module.exports.findAll = (event, context, callback) => {
     }
   });
 };
+
+module.exports.findOne = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const sql = 'SELECT * FROM todos WHERE id = ?';
+  connection.query(sql, [event.pathParameters.todo], (error, row) => {
+    if (error) {
+      callback({
+        statusCode: 500,
+        body: JSON.stringify(error)
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          todos: row
+        })
+      });
+    }
+  });
+};
